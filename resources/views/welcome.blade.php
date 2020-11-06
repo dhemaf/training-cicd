5020852c4b69050149903fb97d59965a2c73848b
dhemalam <50804712+dhemalam@users.noreply.github.com>
2020-11-06 14:04:04 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Dhema Alam
+                    LARAVEL
                 </div>
 
                 <div class="links">
